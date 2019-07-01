546193dad0e4de010b670d06410cdac75995eeb3
HoolyPanda <pavlov.ss10@gmail.com>
2019-07-01 04:18:16 +0300

    stuff

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ import server
 
 
 s = server.Server()
-s.Translate('брат')
+s.Translate(lang='Ru', word='великий брат.')
+a = server.stem()
+v = a.analyze("моего великого брата. jkjkjk")
+
+b = v[0].get("analysis")[0].get('lex')
+
+с = v[2].get("analysis")[0].get('lex')
+
+
+d = v[4].get("analysis")[0].get('lex')
+
+n = a.lemmatize("моего")
+print ("fsd")
