ec9fd6cea459422685ac811978599da89dd9d641
HoolyPanda <pavlov.ss10@gmail.com>
2019-07-09 00:15:46 +0300

    Beta

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-import csv 
 from xlrd import open_workbook
 import server
 
 
 s = server.Server()
-s.Translate(lang='Ru', word='великий брат.')
-a = server.stem()
-v = a.analyze("моего великого брата. jkjkjk")
-
-b = v[0].get("analysis")[0].get('lex')
-
-с = v[2].get("analysis")[0].get('lex')
-
-
-d = v[4].get("analysis")[0].get('lex')
-
-n = a.lemmatize("моего")
-print ("fsd")
+procText = input("Введите текст на Русском: ")
+s.Translate(lang='Ru', word=procText)
--- server.py
+++ server.py
@@ -5,14 +5,11 @@ from pymystem3 import Mystem as stem
 import json
 from xlrd import open_workbook
 import xlrd 
-
+import openpyxl
 
 
 class Server:
     def __init__(self):
-        # self.wordlist = pandas.read_excel("tato-wordlist.xlsx" , sheet_name='Тато - Русский')
-        # print(self.wordlist['Слово'][1])
-        # self.wl = self.wordlist['Слово']
         self.addTranslations = []
         pass
 
@@ -26,7 +23,6 @@ class Server:
             for letter in word:
                 buff += letter
                 i += 1
-                n = len(word)
                 firstRoot = self.__translate__(s.lemmatize(buff)[0])
                 if (i < len(word)):
                     if ((firstRoot is not None) & (word[i] == 'о')):
@@ -74,49 +70,39 @@ class Server:
         if (self.lang == "Ru"):
             word = ""
             for sword in stem().analyze(sent):
-                if (sword.get("text") != " "):
+                if ((sword.get("text") != ".") & (sword.get("text") != " ") & (sword.get("text") != "\n")):
                     word = sword.get("analysis")[0].get('lex')
                     o = self.__translate__(word)
                     if (o):
                         out += (o + "-")
-                elif (sword.get("text") != "."):
-                    word = sword.get("analysis")[0].get('lex')
-                    o = self.__translate__(word)
+                elif ((sword.get("text") != " ") & (sword.get("text") != "\n")):
+                    out = out[0:(len(out) - 1)]
+                    out += sword.get("text") 
+                elif (sword.get("text") == "\n"):
+                    out += "\n"
                 pass
-            for letter in sent:
-                if ((letter != ' ') & (letter != ".")):
-                    word += letter
-                elif ((letter == ".") & (True)):
-                    o = self.__translate__(word)
-                    if (o):
-                        out += self.__translate__(word)
-                    if (len(self.addTranslations) != 0):
-                        for aWord in self.addTranslations:
-                            print(aWord)
-                            wordType = input("      Введите тип слова: ") 
-                            wortdTranslate = input("      Введите перевод слова: ") 
-                            wb = open_workbook("tato-wordlist.xlsx")
-                            sheet = wb.sheet_by_index(0)
-                            rows = sheet.nrows
-                            sheet.write(rows, 0, aWord)
-                            sheet.write(rows, 1, wordType)
-                            sheet.write(rows, 2, wortdTranslate)
-                            
-                            sheet = wb.sheet_by_index(1)
-                            rows = sheet.nrows
-                            sheet.write(rows, 0, wortdTranslate)
-                            sheet.write(rows, 1, wordType)
-                            sheet.write(rows, 2, aWord)
-                    else:
-                        print ( sent + "\n" + out)
-                else:
-                    o = self.__translate__(word)
-                    if (o):
-                        out += (self.__translate__(word)) + "-"
-                        
-                    word = ""
-                
-            pass
+            if (len(self.addTranslations) != 0):
+                for aWord in self.addTranslations:
+                    print("Ctrl + C to quit")
+                    print(aWord)
+                    wordType = input("      Введите тип слова: ") 
+                    wortdTranslate = input("      Введите перевод слова: ") 
+                    wb = openpyxl.load_workbook(filename = "./tato-wordlist.xlsx")
+                    sheet = wb.get_sheet_by_name("Тато - Русский")
+                    rows = sheet.max_row + 1
+                    sheet.cell(rows, 1).value = wortdTranslate
+                    sheet.cell(rows, 2).value = wordType
+                    sheet.cell(rows, 3).value = aWord
+                    sheet = wb.get_sheet_by_name("Русский - Тато")
+                    sheet.cell(rows, 1).value = aWord
+                    sheet.cell(rows, 2).value = wordType
+                    sheet.cell(rows, 3).value = wortdTranslate
+                    wb.save(filename = "./tato-wordlist.xlsx")
+                    wb.close()
+                    
+            else:
+                print ( sent + "\n" + out)
+                pass
         elif (self.lang == "Ta"):
             pass
 
@@ -131,16 +117,16 @@ class Server:
     def Translate(self, lang:str ,word:str):
         self.lang = lang
         if (lang == "Ta"):
-            self.wl = pandas.read_excel("tato-wordlist.xlsx" , sheet_name='Тато - Русский')
+            self.wl = pandas.read_excel("./tato-wordlist.xlsx" , sheet_name='Тато - Русский')
             self.wlIndex = self.wl.index
             if ('-' in word):
                 if ('.' in word):
                     self.__TranslateSentance__(sent=word)
                 else:
-                    self.__TransalteComplexWord__(word=word)
+                    self.__TransalteComplexWord__(word=word)    
                     pass
         elif (lang == 'Ru'):
-            self.wl = pandas.read_excel("tato-wordlist.xlsx" , sheet_name='Русский - Тато')
+            self.wl = pandas.read_excel("./tato-wordlist.xlsx" , sheet_name='Русский - Тато')
             s=stem()
             if (' ' in word):
                 self.__TranslateSentance__(word)
