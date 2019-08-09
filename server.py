540ca58e2b7816a214eae0301442738fbd3d4180
HoolyPanda <pavlov.ss10@gmail.com>
2019-08-09 15:35:57 +0300

    data transmission added

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ import socket
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 a = socket.gethostname()
-serverSocket.bind((socket.gethostname(), 8756))
+serverSocket.bind(("192.168.0.102", 8757))
 serverSocket.listen(1)
 
 while True:
-    a = serverSocket.accept()
+    conn, addr = serverSocket.accept()
 
-    bufferData = serverSocket.recv(32)
+    bufferData = conn.recv(32)
     if bufferData == b"begin":
-        bufferData = serverSocket.recv(1024)
+        bufferData = conn.recv(1024)
         while  bufferData:
-            print("Got: ", bufferData.encode("utf-8"))
-            bufferData = serverSocket.recv(1024)
+            print("Got: ", bufferData.decode("utf-8"))
+            bufferData = conn.recv(1024)
             pass
 
     pass
